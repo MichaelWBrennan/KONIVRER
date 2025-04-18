5bbadb7557d8cd6cfccd0c5b5d3088b382b6d596
Michael <michaelwilliambrennan@gmail.com>
2025-04-18 09:58:47 -0400

    Delete Backend/Models directory

--- Backend/Models/Deck.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const deckSchema = new mongoose.Schema({
-    name: String,
-    cards: Array,
-    owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
-});
-
-module.exports = mongoose.model('Deck', deckSchema);
