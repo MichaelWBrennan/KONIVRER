34a6263785a7558f57581c8172cef2eb0c69c8b1
Michael Brennan <2914065+MichaelWBrennan@users.noreply.github.com>
2025-04-18 08:34:32 -0400

    Add files via upload

---
+++ Backend/Controllers/authController.js
@@ -0,0 +1,28 @@
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+exports.register = async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = await User.create({ username, password: hashedPassword });
+        res.status(201).json({ message: 'User created' });
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+};
+
+exports.login = async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        const user = await User.findOne({ username });
+        if (!user || !(await bcrypt.compare(password, user.password))) {
+            return res.status(401).json({ error: 'Invalid credentials' });
+        }
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1d' });
+        res.json({ token });
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+};
