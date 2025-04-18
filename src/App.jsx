a5a3f51c4ccff5311d757355af219a346afc6658
Crypto3k <michaelwilliambrennan@gmail.com>
2025-04-18 07:17:07 -0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import DeckBuilder from './components/DeckBuilder';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>KONIVRER Deckbuilder</h1>
+      <DeckBuilder />
+    </div>
+  );
+}
+
+export default App;
