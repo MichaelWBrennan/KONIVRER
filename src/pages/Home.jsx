144100b3b04d0b83dfeabce5bf0c97337c97eb7f
Michael Brennan <2914065+MichaelWBrennan@users.noreply.github.com>
2025-05-28 04:07:58 -0400

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import DeckBuilder from "../components/DeckBuilder";
+
+const Home = () => (
+  <div>
+    <h1>KONIVRER Deck Database</h1>
+    <DeckBuilder />
+  </div>
+);
+
+export default Home;
