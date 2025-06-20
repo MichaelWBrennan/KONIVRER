90c1c69e5d6ef6978289339c608227432710dd4d
Michael <michaelwilliambrennan@gmail.com>
2025-06-20 23:57:35 +0000

    style: auto-format code with Prettier

--- src/components/CardDatabase.jsx
+++ src/components/CardDatabase.jsx
@@ -416,9 +416,7 @@ const CardDatabase = ({
               {/* "64 cards total" text removed */}
             </span>
           ) : (
-            <>
-              {/* "Showing 64 of 64 cards" text removed */}
-            </>
+            <>{/* "Showing 64 of 64 cards" text removed */}</>
           )}
         </span>
         {favorites.size > 0 && (
