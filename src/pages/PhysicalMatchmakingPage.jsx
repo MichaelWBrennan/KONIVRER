97e02aad2e7b620ae138bac5e3f766b0854998f0
Michael Brennan <2914065+MichaelWBrennan@users.noreply.github.com>
2025-06-22 04:08:43 +0000

    Implement enhanced physical matchmaking component with QR code generation and tournament management

---
+++ src/pages/PhysicalMatchmakingPage.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import EnhancedPhysicalMatchmaking from '../components/EnhancedPhysicalMatchmaking';
+
+const PhysicalMatchmakingPage = () => {
+  return <EnhancedPhysicalMatchmaking />;
+};
+
+export default PhysicalMatchmakingPage;
