85689b35fdd5bcbce38a5562fe125192c701fcda
Michael Brennan <michaelwilliambrennan@gmail.com>
2025-06-27 10:24:56 +0000

    Pre-Recovery checkpoint

---
+++ replit.nix
@@ -0,0 +1,22 @@
+{pkgs}: {
+  deps = [
+    pkgs.ipscan
+    pkgs.haskellPackages.phonetic-languages-phonetics-basics
+    pkgs.gnomeExtensions.window-calls-extended
+    pkgs.rubyPackages_3_2.forwardable-extended
+    pkgs.rPackages.motifbreakR
+    pkgs.haskellPackages.dependent-map_0_2_4_0
+    pkgs.python311Packages.aionanoleaf
+    pkgs.rPackages.neatR
+    pkgs.rPackages.apportion
+    pkgs.gnomeExtensions.net-speed
+    pkgs.libcardiacarrest
+    pkgs.deepin.dwayland
+    pkgs.emacsPackages.solo-jazz-theme
+    pkgs.cudaPackages.cuda_nvprof
+    pkgs.miniball
+    pkgs.emacsPackages.flycheck-clangcheck
+    pkgs.rPackages.DIME
+    pkgs.rPackages.AllPossibleSpellings
+  ];
+}
