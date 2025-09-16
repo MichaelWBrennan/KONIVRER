744f927ef098f625d92d3f4d5b93b6a9488da8fe
Michael Brennan <michaelwilliambrennan@gmail.com>
2025-09-16 00:38:51 -0400

    Rewrite aether page as 1500ce medical text (#1389)

--- src/types/index.ts
+++ src/types/index.ts
@@ -165,6 +165,7 @@ export {};
 export {};
 export {};
 export {};
+export {};
 // Re-export all types from various type definition files
 export * from "./game";
 
@@ -227,6 +228,9 @@ export interface Card {
   color?: string;
   text?: string;
 }
+declare module "*.css";
+declare module "*.svg";
+declare module "*.png";
 declare module "*.css";
 declare module "*.svg";
 declare module "*.png";
