ba744d368a01eaebad6d1689294e8000340211ac
Michael Brennan <michaelwilliambrennan@gmail.com>
2025-09-27 22:18:56 -0400

    Build project with typescript error (#1455)

--- src/services/api.ts
+++ src/services/api.ts
@@ -177,6 +177,7 @@ export const cardApi = {
       [] =
       [] =
       [] =
+      [] =
         []),
   ) => api.post("/cards/bulk", data),
   getStatistics: () => api.get("/cards/statistics"),
